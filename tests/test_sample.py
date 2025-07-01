688f9b4bd3016e0c18f7251f8ef8afc31dea5f05
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 17:52:15 -0400

    Fix: update test and app response message to match

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,11 +1,9 @@
 import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.app import app
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 
 def test_home():
     response = app.test_client().get("/")
     assert response.status_code == 200
-    assert b"Hello from Flask!" in response.data
+    assert b"Hello, from Flask!" in response.data
