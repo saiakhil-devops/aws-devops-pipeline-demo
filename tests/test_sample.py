19f16d7a241e9b1b07ff7d4ebb10133c7fd6afcb
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 12:01:23 -0400

    style: auto-format test file with black

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,9 +2,12 @@ import sys
 import os
 
 # Add the app folder to the system path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "app"))
+)
 
 from app import app  # Now Python can find app.py inside the app folder
+
 
 def test_home():
     response = app.test_client().get("/")
