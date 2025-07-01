d529f421732aeb1323a9ce48e39dec72307a859c
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 17:57:23 -0400

    Fix: Flake8 lint errors in test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.app import app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 def test_home():
     response = app.test_client().get("/")
