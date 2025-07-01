88141dc293990e693977ff080e135105cb4a5768
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 12:40:37 -0400

    fix: flake8 and import issues in test_sample

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,10 +1,15 @@
 import sys
 import os
 
-# Add the parent directory to Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# Add app directory to Python path
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..')
+    )
+)
 
-from app import app  # <-- this will now work correctly
+from app.app import app
 
 
 def test_home():
