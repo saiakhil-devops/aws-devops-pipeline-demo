fef3c46acac7b8e37b349d311321bf0cd0ae01d9
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 12:48:21 -0400

    fix: lint issues (E501, E302, W292) to pass flake8

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,7 +2,11 @@ from app.app import app
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
+
 
 def test_home():
     response = app.test_client().get("/")
