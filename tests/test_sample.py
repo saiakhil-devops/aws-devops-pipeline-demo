4a161325388da240123390d3679b5332e018c0b9
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 17:39:32 -0400

    fix: reorder imports to satisfy flake8 E402

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,9 +1,8 @@
 import sys
 import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app.app import app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_home():
