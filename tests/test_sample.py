aceab2b874d6fe95f2d76b5ba998272c6a4542d5
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 11:57:26 -0400

    fix: adjust import path in test for GitHub Actions

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,10 @@
-from app.app import app
+import sys
+import os
 
+# Add the app folder to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
+from app import app  # Now Python can find app.py inside the app folder
 
 def test_home():
     response = app.test_client().get("/")
