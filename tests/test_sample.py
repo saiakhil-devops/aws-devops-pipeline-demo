2b5d5d4990f012eb133eecd8c794999873a224ee
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 12:44:15 -0400

    fix: place import at top to resolve flake8 E402

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,8 @@
+from app.app import app
 import sys
 import os
 
-# Add app directory to Python path
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..')
-    )
-)
-
-from app.app import app
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_home():
     response = app.test_client().get("/")
