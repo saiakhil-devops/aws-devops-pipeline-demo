4e299859ab4cd390ad592c1051f903b4a5ac0eb3
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 12:30:36 -0400

    style: format code using black

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,9 +2,10 @@ import sys
 import os
 
 # Add the parent directory to Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app  # <-- this will now work correctly
+
 
 def test_home():
     response = app.test_client().get("/")
