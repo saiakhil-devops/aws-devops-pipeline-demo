07e34e7d0da9772ddb1a8d62d33ef131c167028f
saiakhil-devops <saiakhilperumallaofficial@gmail.com>
2025-07-01 17:42:08 -0400

    chore: auto-format with black

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,7 +2,7 @@ import sys
 import os
 from app.app import app
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def test_home():
